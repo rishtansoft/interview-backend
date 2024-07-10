fa2f7bd2b73ae8b09840acd3f943c083e6d31a6d
rishtansoft <rishtansoft@gmail.com>
2024-07-10 13:11:37 +0500

    create superadmin

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,18 +1,26 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../config/config';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
-  public id!: number;
+  public id!: string;
   public username!: string;
   public email!: string;
+  public role!: string;
+  public password!: string;
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
+
+  public static async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10);
+  }
 }
 
 User.init({
   id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
+    type: DataTypes.UUID,
+    defaultValue: uuidv4,
     primaryKey: true
   },
   username: {
@@ -22,15 +30,45 @@ User.init({
   email: {
     type: DataTypes.STRING,
     allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM,
+    values: ['superadmin', 'admin', 'user'],
+    allowNull: false,
+    defaultValue: 'user'
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
 }, {
   sequelize,
   tableName: 'users',
-  timestamps: true // createdAt va updatedAt uchun
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        user.password = await User.hashPassword(user.password);
+      }
+    }
+  }
 });
 
-sequelize.sync() // Jadvallarni yaratish
-  .then(() => console.log('Users table has been synchronized successfully.'))
+sequelize.sync()
+  .then(async () => {
+    console.log('Users table has been synchronized successfully.');
+    // Superadmin foydalanuvchisini yaratish
+    const superadminExists = await User.findOne({ where: { username: 'superadmin' } });
+    if (!superadminExists) {
+      await User.create({
+        username: 'superadmin',
+        email: 'superadmin@example.com',
+        role: 'superadmin',
+        password: '12345'
+      });
+      console.log('Superadmin user has been created.');
+    }
+  })
   .catch(err => console.error('Error synchronizing the users table:', err));
 
 export default User;
